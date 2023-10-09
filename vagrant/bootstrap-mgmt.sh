784c6db2c6ee1b6c19f892ad7c20820053e52aff
Miguel Encarnação <mjceo@iscte-iul.pt>
2023-10-09 21:01:50 +0100

    [AGISIT-01] IaC Base Commit
    º Added Base Implementation with Google Cloud Connectivity

---
+++ vagrant/bootstrap-mgmt.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install nano less iptables iputils-ping
+
+# install tools for managing ppa repositories
+sudo apt-get -y install software-properties-common
+sudo apt-get -y install unzip
+sudo apt-get -y install build-essential
+sudo apt-get -y install libssl-dev 
+sudo apt-get -y install libffi-dev 
+
+# Add graph builder tool for Terraform
+sudo apt-get -y install graphviz
+
+# install Ansible (http://docs.ansible.com/intro_installation.html)
+sudo apt-add-repository -y -u ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+
+# Install Terraform
+sudo apt-get update
+# install GNU Privacy Guard
+sudo apt-get install -y gnupg
+# add HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt-get -y install terraform
+
+# Install Google Cloud SDK
+snap install google-cloud-sdk --classic
+
+# Install Kubernetes Controller
+sudo apt-get -y install kubectl google-cloud-sdk-gke-gcloud-auth-plugin
+
+# Clean up cached packages
+sudo apt-get clean all
+
