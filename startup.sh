468b015ad919410146aab816ec554503677e2413
Miguel Encarnação <mjceo@iscte-iul.pt>
2023-10-28 17:48:58 +0100

    [AGISIT] Removed Script

--- startup.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-cd /home/vagrant/project/terraform
-terraform apply -auto-approve > outputs
-cd /home/vagrant/project/
-python3 extractor.py
-cd /home/vagrant/project/ansible
-ansible-playbook ansible-gcp-main-playbook.yml
